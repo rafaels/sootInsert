f399f15cd3eb22d546d205742734dbdadeb18708
Rafael Souza <rafael@maisweb.org>
2012-02-03 18:08:31 -0300

    Refatorando

--- src/Main.java
+++ src/Main.java
@@ -12,22 +12,19 @@ public class Main {
 	private static HashMap<Integer, Channel> canais = new HashMap<Integer, Channel>();
 	private static EChannelsEhandlers dadosDesS;
 	
-	static { //escrevendo em dadosDesS o objeto guardado em dados.out com as informações dos canais, contextos e tratadores
-		try {
-			FileInputStream fIn = new FileInputStream("dados.out");
-			ObjectInputStream in = new ObjectInputStream(fIn);
-			dadosDesS = (EChannelsEhandlers)in.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static EChannelsEhandlers getDadosCanais() {
 		return dadosDesS;
 	}
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		processEChannels();
 		PackManager.v().getPack("jtp").add(new Transform("jtp.appletContextsTransformer", AppletContextsTransformer.v()));
 		soot.Main.main(args);
+	}
+	
+	private static void processEChannels() throws IOException, ClassNotFoundException {
+		FileInputStream fIn = new FileInputStream("dados.out");
+		ObjectInputStream in = new ObjectInputStream(fIn);
+		dadosDesS = (EChannelsEhandlers) in.readObject();
 	}
 }
