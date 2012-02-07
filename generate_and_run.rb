deecf0ccb266fec39f0d7484397ab7d5b424fa0a
Rafael Souza <rafael@maisweb.org>
2012-02-07 01:27:28 -0300

    Alterando script de run

--- generate_and_run.rb
+++ generate_and_run.rb
@@ -1,3 +1,3 @@
 puts `ant -f generate-jar.xml`
 puts `java -jar sootInsert.jar client testeBin/NewUserAccess/bin/ -cp /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:testeBin/NewUserAccess/lib/api.jar:testeBin/NewUserAccess/bin/:testeBin/NewUserAccess/lib/javacardframework.jar`
-puts `java -jar sootInsert.jar host testeBin/NewUserAccessHost/bin/ -cp /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:testeBin/NewUserAccessHost/bin/`
+puts `java -jar sootInsert.jar host testeBin/NewUserAccessHost/bin/ -cp /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:testeBin/NewUserAccessHost/bin/:testeBin/handlers/`
